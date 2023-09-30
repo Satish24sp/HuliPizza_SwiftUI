6331b0106681f5e7dd6fe2333146c7634e61c5c9
Satish <35003745+Satish24sp@users.noreply.github.com>
2023-09-30 18:03:13 +0530

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Satish Thakur on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
